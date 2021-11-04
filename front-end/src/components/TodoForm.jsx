c29f1d22b4c7de3c68c3285d8a6a3635d4031c13
legalvao1 <legalvao1@gmail.com>
2021-11-03 22:07:21 -0300

    Configurando Todo form para receber as tarefas

---
+++ front-end/src/components/TodoForm.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const TodoForm = (props) => {
+  const [input, setInput] = useState(
+    props.edit ? props.edit.value : ''
+  );
+
+  const inputRef = useRef(null);
+
+  useEffect(() => inputRef.current.focus());
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+      createdAt: new Date().toLocaleString('en-US'),
+    });
+
+    setInput('')
+  };
+
+  return (
+    <form className='todo-form' onSubmit={ handleSubmit }>
+      <input 
+        placeholder={props.edit ? 'Nova tarefa' : 'Adicione uma tarefa'}
+        value={input}
+        onChange={ handleChange }
+        name='text'
+        className={props.edit ? 'todo-input edit' : 'todo-input'}
+        ref={inputRef}
+      />
+      <button onClick={handleSubmit} className={props.edit ? 'todo-button edit' : 'todo-button'}>
+      {props.edit ? 'Editar' : 'Adicionar'}
+      </button>
+    </form>
+  )
+};
+
+export default TodoForm;
