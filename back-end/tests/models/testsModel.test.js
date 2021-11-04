8e628560d6a88e28fda81441518bf2bfe31e6d30
legalvao1 <legalvao1@gmail.com>
2021-11-04 16:42:11 -0300

    Teste de quando ha tarefas cadastradas no banco

--- back-end/tests/models/testsModel.test.js
+++ back-end/tests/models/testsModel.test.js
@@ -39,5 +39,42 @@ describe('Busca todas as tarefas no banco', () => {
     });
 
   });
+
+  describe('quando existem tarefas cadastradas', () => {
+    const expected_task = {
+      _id: "618434dd1aa7faa987a708c8",
+      id: 455,
+      text: "pular",
+      createdAt: "11/4/2021, 4:30:37 PM",
+      status: "Em andamento"
+    };
+
+    before(async () => {
+        const db = await mongoConnection.connection();
+        await db.collection('todos').insertOne({ ...expected_task });
+    });
+
+    after(async () => {
+        const db = await mongoConnection.connection();
+        await db.collection('todos').drop();
+    });
+
+    it('retorna um array', async () => {
+        const response = await todosModel.getTodos();
+        expect(response).to.be.an('array');
+    });
+
+    it('o array não está vazio', async () => {
+        const response = await todosModel.getTodos();
+
+        expect(response).to.be.not.empty;
+    });
+
+    it('a tarefa cadastrada está na lista', async () => {
+        const [{ _id, id, text, createdAt, status }] = await todosModel.getTodos();
+
+        expect({ _id, id, text, createdAt, status }).to.deep.equal(expected_task);
+    });
+  });
 });
 
