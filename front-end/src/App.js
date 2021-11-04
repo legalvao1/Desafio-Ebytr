8b5c60a2da587a6213a91984550e1775cbba7f6f
legalvao1 <legalvao1@gmail.com>
2021-11-03 22:09:10 -0300

    Colocando a TodoList no app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+// import Todo from './components/Todo'
+import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ebytr Todo-List</h1>
+        <TodoList />
       </header>
     </div>
   );
